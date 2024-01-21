7fa526fbb44837a57afddc97721c982c9445d5c0
AlvesLuis0 <alves.luis.3002@gmail.com>
2024-01-21 14:53:06 -0300

    first commit

---
+++ src/main/java/com/alves/tmpfile/TmpfileApplication.java
@@ -0,0 +1,13 @@
+package com.alves.tmpfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmpfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmpfileApplication.class, args);
+	}
+
+}
